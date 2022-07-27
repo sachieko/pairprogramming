f6e29ecc606651e524a627bf13e2f54200cccae1
sachieko <kirazetsubou@gmail.com>
2022-07-27 17:43:39 -0600

    Finished pair programming on raisin.js and stretch for it too.

---
+++ raisin.js
@@ -0,0 +1,31 @@
+const raisinAlarm = function(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === "🍇") {
+      return "Raisin alert!";
+    }
+  }
+  return "All good!";
+};
+// As a stretch exercise, see if you can make a new function that supports arrays of arrays.
+//  Your function should return an array of responses, instead of a single response.
+//  You can add this new function to your existing raisin.js file.
+
+const raisinAlarmArray = function(cookies) {
+  let returnArray = [];
+  for (let array of cookies) {
+    returnArray.push(raisinAlarm(array));
+  }
+  return returnArray;
+};
+
+console.log(raisinAlarm(["🍫", "🍫", "🍇", "🍫"]));
+console.log(raisinAlarm(["🍫", "🍇", "🍫", "🍫", "🍇"]));
+console.log(raisinAlarm(["🍫", "🍫", "🍫"]));
+
+console.log(
+  raisinAlarmArray([
+    ["🍫", "🍫", "🍇", "🍫"],
+    ["🍫", "🍇", "🍫", "🍫", "🍇"],
+    ["🍫", "🍫", "🍫"],
+  ])
+);
